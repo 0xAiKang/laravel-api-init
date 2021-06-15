840f3ee438f2a91a39ea97724554b6b2a7550e22
Boo <aikangtongxue@gmail.com>
2021-06-15 21:10:08 +0800

    引入 IDE Helper 扩展

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -3,32 +3,47 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Traits\UserTrait;
 
 /**
- * @property integer $id
- * @property string $name
- * @property string $email
- * @property string $email_verified_at
- * @property string $password
- * @property string $remember_token
- * @property string $created_at
- * @property string $updated_at
+ * App\Models\UserModel
+ *
+ * @property int                             $id
+ * @property string                          $name
+ * @property string                          $email
+ * @property string|null                     $email_verified_at
+ * @property string                          $password
+ * @property string|null                     $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null                     $avatar     用户头像
+ * @property int                             $is_deleted 是否删除
+ * @method static \Illuminate\Database\Eloquent\Builder|UserModel newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserModel newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserModel query()
+ * @method static \Illuminate\Database\Eloquent\Builder|UserModel whereAvatar($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserModel whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserModel whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserModel whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserModel whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserModel whereIsDeleted($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserModel whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserModel wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserModel whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|UserModel whereUpdatedAt($value)
+ * @mixin \Eloquent
  */
 class UserModel extends Model
 {
-    use UserTrait;
-
     /**
      * The table associated with the model.
-     * 
+     *
      * @var string
      */
     protected $table = 'users';
 
     /**
      * The "type" of the auto-incrementing ID.
-     * 
+     *
      * @var string
      */
     protected $keyType = 'integer';
@@ -36,6 +51,14 @@ class UserModel extends Model
     /**
      * @var array
      */
-    protected $fillable = ['name', 'email', 'email_verified_at', 'password', 'remember_token', 'created_at', 'updated_at'];
+    protected $fillable = [
+        'name',
+        'email',
+        'email_verified_at',
+        'password',
+        'remember_token',
+        'created_at',
+        'updated_at'
+    ];
 
 }
