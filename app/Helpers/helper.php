9f9a7d7794b5fe2ae2ea47d0100f2dff819a74c5
Boo <aikangtongxue@gmail.com>
2021-06-15 22:04:11 +0800

    更新助手函数库

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -3,8 +3,15 @@
  * 助手函数库
  */
 
-if (!function_exists("page")) {
-    function page($data)
+if (!function_exists("get_page")) {
+    /**
+     * 返回客户端分页相关信息
+     *
+     * @param $data
+     *
+     * @return array
+     */
+    function get_page($data)
     {
         return [
             'row_count' => $data->count(),                  //当前页条数
@@ -13,5 +20,35 @@ if (!function_exists("page")) {
             'total'     => $data->total(),                  //总数
             'is_more'   => (int)$data->hasMorePages(),      //是否有更多
         ];
+    }
+}
+
+if (!function_exists("get_item")) {
+    /**
+     * 返回客户端数据
+     *
+     * @param $data
+     *
+     * @return array
+     */
+    function get_item($data)
+    {
+        return $data->items();
+    }
+}
+
+if (!function_exists("dda")) {
+    /**
+     * dd to array
+     *
+     * @param $model
+     */
+    function dda($model)
+    {
+        if (method_exists($model, 'toArray')) {
+            dd($model->toArray());
+        } else {
+            dd($model);
+        }
     }
 }
